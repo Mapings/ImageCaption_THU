f19c67656be44c91672c122dfae5f26cf807bc9f
doushan12 <dous12@126.com>
2017-05-15 12:02:53 +0800

    输入改为numpy

--- doushan/input.py
+++ doushan/input.py
@@ -1,17 +1,17 @@
 import h5py
+import numpy as np
 import tensorflow as tf
+
 #将训练数据作为不变变量读入
-config = tf.ConfigProto()
-config.gpu_options.allow_growth = True
-session = tf.Session(config=config)
 
-
-with tf.device('/gpu:0'):
-    f=h5py.File('fc1.h5','r')
-    training_data =f['train_set']
-
-    with tf.Session() as sess:
-     data_initializer = tf.placeholder(dtype=training_data.dtype,shape=training_data.shape)
+#with tf.device('/gpu:0'):
+f=h5py.File('fc1_new.h5','r')
+training_data =np.array(f['train_set'],dtype=np.int32)
+training_data =training_data.transpose()
+print (training_data.dtype)
+print (training_data.shape)
+with tf.Session() as sess:
+    data_initializer = tf.placeholder(dtype=training_data.dtype,shape=training_data.shape)
     input_data = tf.Variable(data_initializer, trainable=False, collections=[])
     sess.run(input_data.initializer,feed_dict={data_initializer: training_data})
-    f.close()
+f.close()
