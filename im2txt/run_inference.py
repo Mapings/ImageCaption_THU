760571742fdd6643f28d7857b8e6445085a3c55a
Mapings <mps2012@yeah.net>
2017-05-26 23:34:19 +0800

    更改写txt的编码为utf-8

--- im2txt/run_inference.py
+++ im2txt/run_inference.py
@@ -1,3 +1,6 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
 # Copyright 2016 The TensorFlow Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -32,10 +35,11 @@ from im2txt.inference_utils import vocabulary
 
 import numpy as np
 import h5py
+import codecs
 
 FLAGS = tf.flags.FLAGS
 
-tf.flags.DEFINE_string("checkpoint_path", "my_model2/model.ckpt-899999",
+tf.flags.DEFINE_string("checkpoint_path", "all_model/submit_zi_fc1_969999/model.ckpt-969999",
                        "Model checkpoint file or directory containing a "
                        "model checkpoint file.")
 tf.flags.DEFINE_string("vocab_file", "data/wordlac_count_all.txt", "Text file containing the vocabulary.")
@@ -88,40 +92,38 @@ def main(_):
 
     # 将infer数据作为不变变量读入
     f = h5py.File(FLAGS.input_files, 'r')
-    #infer_data = np.array(f['validation_set'], dtype=np.float32)
     infer_data = np.array(f[FLAGS.input_category], dtype=np.float32)
 
     image_embeddings = infer_data
     f.close()
 
     submit = []
-    for image_idx in range(1000):                             #只测试1000个，即使是训练集也是，后续顺利的话可以重写
-      image_embedding = image_embeddings[image_idx]           # A float32 np.array with shape [embedding_size]
-      # print(image_embedding)
-      captions = generator.beam_search(sess, image_embedding)  #submit的时候，设置，beam_size=1！！！wrong
-      picture_id = str(image_idx+9000)      #这里的image_idx可以加9000用于和图片标号对应
+    for image_idx in range(1000):                              #只测试1000个
+      image_embedding = image_embeddings[image_idx]            # A float32 np.array with shape [embedding_size]
+      captions = generator.beam_search(sess, image_embedding)  
+      picture_id = str(image_idx+9000)                         #这里的image_idx可以加9000用于和图片标号对应
       # print(picture_id)
       select_first = []
       for i, caption in enumerate(captions):
         # Ignore begin and end words.
         sentence = [vocab.id_to_word(w) for w in caption.sentence[1:-1]]
-        if len(vocab.vocab)<3000:
-          sentence = " ".join(sentence)           # 分字
-          caption = "%s %s" % (picture_id, sentence)   #分字
-        else:
-          sentence = "".join(sentence)         # 分词，词之间直接相连
-          sentence = str(list(sentence))   #分词
-          s = sentence[1:-1]               #分词
-          s = s.replace(',','')            #分词
-          s = s.replace("'",'')            #分词
-          caption = "%s %s" % (picture_id, s)          #分词
+        if len(vocab.vocab)<3000:                              # 分字
+          sentence = " ".join(sentence)           
+          caption = "%s %s" % (picture_id, sentence) 
+        else:                                                  # 分词，词之间直接相连
+          sentence = "".join(sentence)         
+          sentence = str(list(sentence))  
+          s = sentence[1:-1]               
+          s = s.replace(',','')            
+          s = s.replace("'",'')            
+          caption = "%s %s" % (picture_id, s)
+        
         select_first.append(caption)
-      # print(select_first)
       submit.append(select_first[0])
-    # print(submit)
-    # open('Results/inference_captions.txt', 'w').write('%s' % '\n'.join(infer_captions)) 
-    open('Results/submit_lzg_8.txt', 'w').write('%s' % '\n'.join(submit)) 
-
+      print(select_first[0])
+    with codecs.open('Results/submit_zi_fc1_969999.txt', 'w', encoding='utf-8') as file:
+      submit_str = '\n'.join(submit)
+      file.write(submit_str)
 
 if __name__ == "__main__":
   tf.app.run()
